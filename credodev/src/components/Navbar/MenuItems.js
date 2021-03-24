08eff50efb35df9c41044e648ba6a345215f232b
Congo Strange <congo@congos-mbp.lan>
2021-03-24 14:45:42 -0700

    changed menu text

---
+++ credodev/src/components/Navbar/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'All Campaigns',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Your Personal Wallet',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Become a Campaign Organizer',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
---
+++ credodev/src/components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { MenuItems } from "./MenuItems"
+import { Button } from "../Button"
+import './Navbar.css'
+// import logo from 'logo.svg';
+
+class Navbar extends Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
+    render() {
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">CREDO</h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                    {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <Button>Login or Sign Up</Button>
+            </nav>
+        )
+    }
+}
+
+export default Navbar
