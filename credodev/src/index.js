14a125b7ee5621c23e59d9963cbd812bed14b073
Congo Strange <congo@congos-mbp.lan>
2021-03-29 11:33:58 -0700

    [WIP] adding campaign creation form

--- credodev/src/index.js
+++ credodev/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import { useForm } from "react-hook-form";
+// import Headers from "./Header";
 
 
 
@@ -9,3 +11,27 @@ import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
+
+//
+// export default function credo() {
+//     const {register, handleSubmit} = useForm();
+//     const onSubmit = (data) => alert(JSON.stringify(data));
+//
+//     return (
+//         <form onSubmit={handleSubmit(onSubmit)}>
+//             <Headers/>
+//
+//             <input name="firstName" ref={register} placeholder="First name"/>
+//
+//             <input name="lastName" ref={register} placeholder="Last name"/>
+//
+//             <select name="Type of Campaign" ref={register}>
+//                 <option value="Environmental">Select...</option>
+//                 <option value="Social Movement">Category A</option>
+//                 <option value="Other">Category B</option>
+//             </select>
+//
+//             <input type="submit"/>
+//         </form>
+//     );
+// }
